0b30e17c86033df84e74c22b74cd1712bbad58fc
yvanzo <yvanzo@metabrainz.org>
2018-06-27 17:51:55 +0200

    Move to transient musicbrainz-docker repository
    https://github.com/yvanzo/musicbrainz-docker is just a few patches
    ahead of official https://github.com/jsturgis/musicbrainz-docker
    
    Most tasks (cloning repositories, installing dependencies, and so
    on) are better handled by Docker Compose file of that repository.

--- install-musicbrainz.sh
+++ install-musicbrainz.sh
@@ -9,55 +9,18 @@ cd musicbrainz
 
 # Clone the musicbrainz-docker repo
 if [[ ! -d "musicbrainz-docker" ]]; then
-    git clone https://github.com/mayhem/musicbrainz-docker.git
+    git clone https://github.com/yvanzo/musicbrainz-docker.git
 else
     cd musicbrainz-docker
     git pull origin master
     cd -
 fi
 
-if [[ ! -d "search-server" ]]; then
-    git clone https://github.com/metabrainz/search-server.git
-else
-    cd search-server
-    git pull origin master
-    cd -
-fi
-
-cd ~/musicbrainz/musicbrainz-docker/indexer-dockerfile
-if [[ ! -e "index.jar" ]]; then
-    echo "Downloading search indexer"
-    curl -o index.jar http://ftp.musicbrainz.org/pub/musicbrainz/search/index/index.jar
-fi
-
-cd ~/musicbrainz/musicbrainz-docker/search-dockerfile
-if [[ ! -e "searchserver.war" ]]; then
-    echo "Downloading server server"
-    curl -o searchserver.war http://ftp.musicbrainz.org/pub/musicbrainz/search/servlet/searchserver.war
-fi
-
-# setup to use yarn
-sudo apt-get install -y nodejs npm
-sudo sh -c 'curl -sL https://deb.nodesource.com/setup_6.x | bash'
-sudo npm install -g yarn
-
-# clone the repo so it can live in the VM
-
-cd ~/musicbrainz/musicbrainz-docker/musicbrainz-dockerfile
-if [[ ! -d "musicbrainz-server" ]]; then
-    git clone https://github.com/metabrainz/musicbrainz-server.git
-fi
-
 if [[ ! -e "~/musicbrainz-server" ]]; then
     ln -s ~/musicbrainz/musicbrainz-docker/musicbrainz-dockerfile/musicbrainz-server ~/musicbrainz-server
 fi
 
-cd musicbrainz-server
-cp ../DBDefs.pm lib
-yarn install
-
-cd ../..
-
+cd musicbrainz-docker
 sudo docker-compose build
 sudo docker-compose up -d
 cd 
