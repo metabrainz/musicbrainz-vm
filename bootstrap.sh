1f9faed1f9b33cfe6f6370989474a3f6686ae5ac
yvanzo <yvanzo@metabrainz.org>
2018-07-05 08:32:22 +0200

    Upgrade docker and docker-compose
    which both are stock packages since Debian Stretch/Ubuntu Xenial.
    
    (`ubuntu` user does not even exist anymore.)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# install docker
-command -v docker >/dev/null 2>&1
-rc=$?
-if [[ $rc != "0" ]]; then
-    curl -sSL https://get.docker.com/ | sh
-
-    # add ubuntu user to docker group
-    sudo usermod -aG docker ubuntu
-fi
-
-# install docker-compose
-command -v docker-compose >/dev/null 2>&1
-rc=$?
-if [[ $rc != "0" ]]; then
-    curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-fi
+# install docker and docker-compose
+apt-get install -y docker.io docker-compose
 
 # copy the helper scripts from the musicbrainz-vm repo locally
 mkdir -p bin
