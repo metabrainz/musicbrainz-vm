49c4d32a80a7870a529b606b582ebb4ecb60d931
yvanzo <yvanzo@metabrainz.org>
2018-07-09 14:56:44 +0200

    Add script to turn service port on/off in the VM
    > Usage: bin/turn-port <db|musicbrainz|redis|search> <on|off>
    
    First argument matches both Docker Compose service name and
    VirtualBox port forwarding rule name.
    
    Thus, turning a port on in the VM forwards it outside the VM.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,7 @@ apt-get install -y docker.io docker-compose
 # copy the helper scripts from the musicbrainz-vm repo locally
 mkdir -p bin
 cp -r /vagrant/bin/* bin
+mkdir -p lib
+cp -r /vagrant/lib/* lib
 
 echo "Basic setup of the VM is complete. (bootstrap)"
