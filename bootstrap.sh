b1e7769cf2929069ea3fadc801d43b15a05da863
yvanzo <yvanzo@metabrainz.org>
2018-07-25 15:00:52 +0200

    Copy documentation VM-USAGE.md into the VM

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,7 @@ apt-get install -y docker.io docker-compose lua5.2 lua-yaml
 mkdir -p bin
 cp -r /vagrant/bin/* bin
 
+# copy the doc that explains how to use the generated VM
+cp --preserve=timestamps /vagrant/VM-USAGE.md VM-USAGE.md
+
 echo "Basic setup of the VM is complete. (bootstrap)"
