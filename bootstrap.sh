670130d811e6f4d5368601e50e5f030c74e12b49
yvanzo <yvanzo@metabrainz.org>
2018-11-13 11:55:40 +0100

    Set ownership of helper scripts and doc to vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,11 @@ apt-get install -y docker.io docker-compose lua5.2 lua-yaml
 # copy the helper scripts from the musicbrainz-vm repo locally
 mkdir -p bin
 cp -r /vagrant/bin/* bin
+chown -R vagrant.vagrant bin
 
 # copy the doc that explains how to use the generated VM
 cp --preserve=timestamps /vagrant/VM-USAGE.md VM-USAGE.md
+chown vagrant.vagrant VM-USAGE.md
 
 # copy the script that prints MusicBrainz text at login
 cp --preserve=mode,timestamps {/vagrant/provision,}/etc/updated-motd.d/11-musicbrainz-text
