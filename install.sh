7a0ba7f7160cf01ae45b6caafda60f5709cc7443
yvanzo <yvanzo@metabrainz.org>
2018-12-13 09:24:07 +0100

    Set exit status of failed install to error (1)

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ PG_DATA_FILE=./pg-data.vdi
 vagrant up --no-provision
 if [[ $? != "0" ]]; then
     echo "creating the VM failed."
-    exit
+    exit 1
 fi
 
 VMID=`vboxmanage showvminfo musicbrainz-vm --machinereadable | grep ^UUID | grep -o '[a-fA-F0-9]\{8\}-[a-fA-F0-9]\{4\}-[a-fA-F0-9]\{4\}-[a-fA-F0-9]\{4\}-[a-fA-F0-9]\{12\}'`
 echo "VM UUID is $VMID"
 if [[ $VMID == "" ]]; then
     echo "Unable to detect the VM UUID"
-    exit
+    exit 1
 fi
 
 # if not exists, vboxmanage closemedium ad26ad5c-b19e-46ba-bee6-88c4e82d7ec7 --delete
@@ -25,39 +25,39 @@ VBoxManage createmedium disk --filename $PG_DATA_FILE --format VDI --size $VMSIZ
 ERR=$?
 if [[ $ERR != "0" ]]; then
     echo "creating postgres medium failed. Error $ERR"
-    exit
+    exit 1
 fi
 
 vagrant halt
 if [[ $? != "0" ]]; then
     echo "stopping the VM to attach a new disk failed"
-    exit
+    exit 1
 fi
 
 VBoxManage storageattach $VMID --storagectl 'SATA Controller' --port 1 --device 0 --type hdd --medium $PG_DATA_FILE
 if [[ $? != "0" ]]; then
     echo "attaching postgres medium failed."
-    exit
+    exit 1
 fi
 
 vagrant up --provision
 if [[ $? != "0" ]]; then
     echo "provisioning the VM failed."
-    exit
+    exit 1
 fi
 
 echo "Basic setup of the VM"
 vagrant ssh -- sudo /vagrant/bootstrap.sh
 if [[ $? != "0" ]]; then
     echo "setting basics of the VM failed"
-    exit
+    exit 1
 fi
 
 echo "Create partitions and filesystem for postgres drive"
 vagrant ssh -- sudo /vagrant/make-docker-fs.sh
 if [[ $? != "0" ]]; then
     echo "create partition and filesystem for docker volumes failed"
-    exit
+    exit 1
 fi
 
 MBVM_RELEASE=`git describe --always --broken --dirty`
@@ -65,17 +65,17 @@ echo "Set MusicBrainz Virtual Machine version to ${MBVM_RELEASE-[MISSING RELEASE
 vagrant ssh -- sudo /bin/bash -c "echo MBVM_RELEASE=$MBVM_RELEASE > /etc/mbvm-release"
 if [[ $? != "0" ]]; then
     echo "Setting the MusicBrainz Virtual Machine release info failed."
-    exit
+    exit 1
 fi
 
 vagrant ssh -- /vagrant/install-musicbrainz.sh
 if [[ $? != "0" ]]; then
     echo "installing the MusicBrainz software failed."
-    exit
+    exit 1
 fi
 
 vagrant ssh -- /vagrant/load-data.sh $FTP_SERVER
 if [[ $? != "0" ]]; then
     echo "Loading the MusicBrainz data failed."
-    exit
+    exit 1
 fi
