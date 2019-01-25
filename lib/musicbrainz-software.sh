afb48ce7853bb4099c22b739bf808be4fe743fd4
yvanzo <yvanzo@metabrainz.org>
2019-01-26 00:42:56 +0100

    Complete 30402c06d72f184c06e6edc44ff87a4f5de5c474

--- lib/musicbrainz-software.sh
+++ lib/musicbrainz-software.sh
@@ -30,7 +30,6 @@ if [[ $COUNT == "0" ]]; then
     (crontab -l ; echo "0 3 * * * /home/vagrant/bin/replicate cron >> /home/vagrant/replication.log") | crontab -
 fi
 ./bin/replicate stop
-./bin/update-reindexes start
 
 echo "Installation of MusicBrainz software is complete."
 echo 
