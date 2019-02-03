88266387b64be08ef0c86bd8026f92965784e007
yvanzo <yvanzo@metabrainz.org>
2019-02-04 00:08:15 +0100

    fix mbvm release version

--- install.sh
+++ install.sh
@@ -100,7 +100,7 @@ fi
 
 MBVM_RELEASE=`git describe --always --broken --dirty`
 echo "Set MusicBrainz Virtual Machine version to ${MBVM_RELEASE-[MISSING RELEASE INFO]}"
-vagrant ssh -- sudo /bin/bash -c "echo MBVM_RELEASE=$MBVM_RELEASE > /etc/mbvm-release"
+vagrant ssh -- "sudo /bin/bash -c 'echo MBVM_RELEASE=$MBVM_RELEASE > /etc/mbvm-release'"
 if [[ $? != "0" ]]; then
     echo "Setting the MusicBrainz Virtual Machine release info failed."
     exit 1
